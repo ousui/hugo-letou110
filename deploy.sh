34d284736ad0b07b2c92304bac47c1267ca8e6e9
shuai.w <x@ousui.org>
2017-09-30 09:23:18 +0800

    update blog for 201709300923

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 # 添加更改
+echo [log] commit modified files......
 git add . 
-
 log_date=`date +%Y%m%d%H%M`
-
 git commit -m "update blog for $log_date"
-
 git push -u origin master
+echo [log] modifieds pushed, now update blog......
+ssh letou110.win ./update-letou110.sh 
 
-ssh letou110.win ./update-letou110.sh &
-
-echo done
+echo [log] done, action done!
