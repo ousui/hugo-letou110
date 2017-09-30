e0271bd05e7f586683a35e2c38ff2a040f4331c3
shuai.w <x@ousui.org>
2017-09-30 09:20:08 +0800

    update blog for 201709300920

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
+# 添加更改
+git add . 
 
+log_date=`date +%Y%m%d%H%M`
+
+git commit -m "update blog for $log_date"
+
+git push -u origin master
+
+ssh letou110.win ./update-letou110.sh &
+
+echo done
